e06e0ec33418aca359101e71f65c0926ee500e1f
rehaboffice <rehabtoffice@gmail.com>
2025-06-18 16:42:10 +0500

    user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # Create your tests here.
 class UserManagersTests(TestCase):
@@ -28,3 +29,28 @@ class UserManagersTests(TestCase):
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
         self.assertTrue(admin_user.is_active)
+
+class SignUpViewTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_signup_view_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse('signup'),
+            {
+                "username": "testuser",
+                "email": "testuser@gmail.com",
+                "password1": "testpassword",
+                "password2": "testpassword",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.get().username, 'testuser')
+        self.assertEqual(get_user_model().objects.all()[0].email, "testuser@gmail.com")
